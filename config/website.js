a9644cfabcee0adb2aa5b0d8c462ff50c129390b
Franklin Christuraj <christuraj.anto@gmail.com>
2019-07-07 15:33:13 +0200

    SEO, social media handles update

--- config/website.js
+++ config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Franklin - AI-driven Product Manager', // Navigation and Site Title
+  siteTitleAlt: 'Franklin', // Alternative Site title for SEO
+  siteTitleShort: 'Franklin', // short_name for manifest
+  siteHeadline: 'I bring useful products to life.', // Headline for schema.org JSONLD
+  siteUrl: 'https://www.franklinchristuraj.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Franklin Christuraj portfolio',
+  author: 'Franklin', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@frankChristuraj', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: '',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
